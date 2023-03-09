2c1ac6f11c565328614fe2faf2efc92920e748ed
xinyaoli <li651854292@gmail.com>
2023-03-09 14:16:32 -0800

    Added a loading page in Intelligent-Service

--- Intelligent-Service/intelligent/intelligentController.py
+++ Intelligent-Service/intelligent/intelligentController.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 from flask import Flask, jsonify, request, render_template, redirect,session
 from intelligent.predictionAlgroithms import IntelligentService
 
@@ -13,14 +16,25 @@ def predict():
         ticker = request.form.get('ticker')
         start = request.form.get('start')
         end = request.form.get('end')
-        price = intelligent_service.prediction(ticker,start,end)
-        session['price'] = price
-        return redirect('/intelligent/result')
+        session['ticker'] = ticker
+        session['start'] = start
+        session['end'] = end
+        return redirect('/intelligent/loading')
+@app.route('/intelligent/loading',methods=['GET'])
+def loading():
+    return render_template('loading.html')
 
 @app.route('/intelligent/result', methods=['GET'])
 def result():
-    price = session.get('price')
+    ticker = session.get('ticker')
+    start = session.get('start')
+    end = session.get('end')
+    price = intelligent_service.prediction(ticker, start, end)
     return render_template('result.html',price=price)
+
+def run_prediction(ticker,start,end):
+    price = intelligent_service.prediction(ticker, start, end)
+    session['price'] = price
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0',port=8084)
