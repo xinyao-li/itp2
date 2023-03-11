85bf8fd798588ea16b5a621920763559efe1c439
xinyaoli <li651854292@gmail.com>
2023-03-10 19:43:54 -0800

    Added redis cache for store intelliegnt-service input

--- API-Gateway-Zuul/build.sh
+++ API-Gateway-Zuul/build.sh
@@ -4,4 +4,4 @@ mvn clean clean package
 
 docker build -t api-gateway-zuul-image .
 
-docker run -p 8080:8080 api-gateway-zuul-image --stock.address=192.168.1.67 --intelligent.address=192.168.1.67
+docker run -p 8080:8080 api-gateway-zuul-image --local.address=192.168.1.67
